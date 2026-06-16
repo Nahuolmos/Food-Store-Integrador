adff9b60578c71cdf3b9ea8235b9ec1617ab2804
facucabrerautn-maker <facu.cabrera.utn@gmail.com>
2026-06-16 18:21:44 -0300

    Creacion de clases Base, Rol y Usuario (Constructores, Getters/Setters y ToString)

--- src/entities/Base.java
+++ src/entities/Base.java
@@ -1,5 +1,32 @@
 package entities;
 
-public class Base {
-    
+import java.time.LocalDateTime;
+
+public abstract class Base {
+    private static Long contador = 1L;
+    protected Long id;
+    protected boolean eliminado;
+    protected LocalDateTime createdAt;
+
+    public Base() {
+        this.id = contador++;
+        this.eliminado = false;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
