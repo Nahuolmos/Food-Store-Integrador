4a9683c8b085c65c9994054f65bf7de886305e91
facucabrerautn-maker <facu.cabrera.utn@gmail.com>
2026-06-16 18:21:16 -0300

    Creacion de clases Base, Rol y Usuario (Constructores, Getters/Setters y ToString)

--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -1,5 +1,79 @@
 package entities;
 
-public class Usuario {
+import enums.Rol;
+
+public class Usuario extends Base {
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private String celular;
+    private String contrasena;
+    private Rol rol;
+
+    public Usuario(String nombre, String apellido, String mail, String celular, String contrasena, Rol rol) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.celular = celular;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", mail=" + mail + ", celular=" + celular + ", contrasena=" + contrasena + ", rol=" + rol + '}';
+    }
+    
+    
+    
     
 }
