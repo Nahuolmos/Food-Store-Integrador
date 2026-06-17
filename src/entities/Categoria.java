c2d8c72c63621834ed0bd1191cae51911259e782
Rafa Ruiz <ri.ruiz@alumno.um.edu.ar>
2026-06-17 20:43:23 -0300

    añadida clase Categoria y Producto

--- src/entities/Categoria.java
+++ src/entities/Categoria.java
@@ -1,5 +1,57 @@
 package entities;
 
-public class Categoria {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Categoria extends Base {
+
+    private String nombre;
+    private String descripcion;
+    private List<Producto> productos = new ArrayList<>();
+
+    public Categoria(String nombre, String descripcion) {
+        super();
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Producto> getProductos() {
+        return Collections.unmodifiableList(productos);
+    }
+
+    public void addProducto2Categoria(Producto producto) {
+        if (producto != null && !productos.contains(producto)) {
+            productos.add(producto);
+        } else {
+            System.out.println("El producto " + producto + " no pudo ser ingresado.");
+        }
+    }
+
+    public void removeProductoFromCategoria(Producto producto) {
+        if (producto != null && !productos.contains(producto)) {
+            productos.remove(producto);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "nombre=" + nombre + ", descripcion=" + descripcion + ", productos=" + productos + '}';
+    }
 }
--- src/entities/Producto.java
+++ src/entities/Producto.java
@@ -1,5 +1,79 @@
 package entities;
 
-public class Producto {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Producto extends Base {
+
+    private String nombre;
+    private Double precio;
+    private String descripcion;
+    private int stock;
+    private String imagen;
+    private Boolean disponible;
+    private Categoria categoria;
+
+    private List<DetallePedido> detallePedidos = new ArrayList<>();
+
+    public Producto(String nombre, Double precio, String descripcion, int stock, String imagen, Boolean disponible, Categoria categoria) {
+        super();
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.disponible = disponible;
+        this.imagen = imagen;
+        this.categoria = categoria;
+
+    }
+
+    public void addDetallePedido(DetallePedido detallePedido) {
+        if (detallePedido != null && !detallePedidos.contains(detallePedido)) {
+            detallePedidos.add(detallePedido);
+        }
+    }
+
+    public void removeDetallePedido(DetallePedido detallePedido) {
+        if (detallePedido != null && !detallePedidos.contains(detallePedido)) {
+            detallePedidos.remove(detallePedido);
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public List<DetallePedido> getDetallePedidos() {
+        return Collections.unmodifiableList(detallePedidos);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", precio=" + precio + ", descripcion=" + descripcion + ", stock=" + stock + ", imagen=" + imagen + ", disponible=" + disponible + ", categoria=" + categoria + ", detallePedidos=" + detallePedidos + '}';
+    }
 }
